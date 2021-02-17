47e591e79b929e20720b4cd1741fea3e75f1b95b
Emerson Luiz <emerson_egl@hotmail.com>
2021-02-17 15:16:31 -0300

    Add admin navbar

---
+++ front-web/src/pages/Catalog/components/Navbar/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './styles.scss';
+
+const Navbar = () => (
+    <nav className="admin-nav-container">
+        <ul>
+            <li>
+                <a href="link">Meus Produtos</a>
+            </li>
+            <li>
+                <a href="link">Minhas Categorias</a>
+            </li>
+            <li>
+                <a href="link">Meus Usuários</a>
+            </li>
+        </ul>
+    </nav>
+);
+export default Navbar;
