d984a99bdab9f86fdadf4613e7530d720bfed51c
Emerson Luiz <emerson_egl@hotmail.com>
2021-03-11 17:24:43 -0300

    UserUpdateValidator

--- backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
+++ backend/src/main/java/com/devsuperior/dscatalog/resources/UserResource.java
@@ -21,6 +21,7 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.devsuperior.dscatalog.dto.UserDTO;
 import com.devsuperior.dscatalog.dto.UserInsertDTO;
+import com.devsuperior.dscatalog.dto.UserUpdateDTO;
 import com.devsuperior.dscatalog.services.UserService; 
 
 @RestController
@@ -60,8 +61,8 @@ public class UserResource {
 	}
 	
 	@PutMapping(value = "/{id}")
-	public ResponseEntity<UserDTO> update(@PathVariable Long id,@Valid  @RequestBody UserDTO dto){
-		dto = service.update(id, dto);
+	public ResponseEntity<UserDTO> update(@PathVariable Long id,@Valid  @RequestBody UserUpdateDTO dto){
+		UserDTO newdto = service.update(id, dto);
 		return ResponseEntity.ok().body(dto);
 	}
 	
